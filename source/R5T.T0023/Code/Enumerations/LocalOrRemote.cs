89d6ce4a40516a7374bdb12da56071f91a8e65cc
David Coats <david@getsafetycone.com>
2021-05-26 13:24:57 -0700

    Add LocalOrRemote enumeration and extensions.

---
+++ source/R5T.T0023/Code/Enumerations/LocalOrRemote.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace R5T.T0023
+{
+    public enum LocalOrRemote
+    {
+        Local = 0,
+        Remote
+
+        // No unknown value (no ambiguity allowed).
+        // No trailing comma since these are the full universe of allowed values.
+    }
+}
---
+++ source/R5T.T0023/Code/Extensions/LocalOrRemoteExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using R5T.T0023;
+
+
+namespace System
+{
+    public static class LocalOrRemoteExtensions
+    {
+        public static T Switch<T>(this LocalOrRemote localOrRemote, Func<T> ifLocal, Func<T> ifRemote)
+        {
+            // Note, this uses the fact that local-or-remote only has two options.
+            var output = localOrRemote == LocalOrRemote.Local
+                ? ifLocal()
+                : ifRemote();
+
+            return output;
+        }
+    }
+}
