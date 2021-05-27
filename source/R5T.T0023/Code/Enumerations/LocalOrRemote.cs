1631cb1c7a50947d48057370c42c6254d0aa8f6e
David Coats <david@getsafetycone.com>
2021-05-27 16:35:45 -0700

    Add documentation.

--- source/R5T.T0023/Code/Enumerations/LocalOrRemote.cs
+++ source/R5T.T0023/Code/Enumerations/LocalOrRemote.cs
@@ -3,6 +3,12 @@
 
 namespace R5T.T0023
 {
+    /// <summary>
+    /// Indicates whether a server resource is local or remote.
+    /// </summary>
+    /// <remarks>
+    /// See prior work in R5T.Portsmouth: https://github.com/SafetyCone/R5T.Portsmouth/blob/master/source/R5T.Portsmouth/Code/Enumerations/LocalOrRemote.cs
+    /// </remarks>
     public enum LocalOrRemote
     {
         Local = 0,
